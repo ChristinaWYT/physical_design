eb6701931d108f5e8301ea3042c6e09eaa0826b1
abhishek <abhishek1015@gmail.com>
2014-06-25 18:56:07 -0700

    plot throughput

---
+++ results/experiment1/plot_throughputs.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as pyplot
+import numpy as np
+
+load_throughputs = []
+rr_throughputs = []
+scan_throughputs = []
+
+for i in range(1,10):
+	with open('results/experiment1/load_' + str(i) + '.dat', 'r') as file:
+		for line in file:
+			words = line.split()
+			if len(words) == 3:
+				if words[0] == '[OVERALL],' and words[1] == 'Throughput(ops/sec),':
+					load_throughputs.append(words[2])
+
+for i in range(1,10):
+	with open('results/experiment1/random_read_' + str(i) + '.dat', 'r') as file:
+		for line in file:
+			words = line.split()
+			if len(words) == 3:
+				if words[0] == '[OVERALL],' and words[1] == 'Throughput(ops/sec),':
+					rr_throughputs.append(words[2])
+	
+
+for i in range(1,10):
+	with open('results/experiment1/scan_' + str(i) + '.dat', 'r') as file:
+		for line in file:
+			words = line.split()
+			if len(words) == 3:
+				if words[0] == '[OVERALL],' and words[1] == 'Throughput(ops/sec),':
+					scan_throughputs.append(words[2])
+	
+print(*load_throughputs, sep=',', end='\n')
+print(*rr_throughputs, sep=',', end='\n')
+print(*scan_throughputs, sep=',', end='\n')
+
+pyplot.plot(range(1,10), load_throughputs, color='r', marker='o', label="load")
+pyplot.plot(range(1,10), rr_throughputs, color='b', marker='o', label="random read")
+pyplot.plot(range(1,10), scan_throughputs, color='g', marker='o', label="scan")
+
+pyplot.ylabel('Throughput')
+
+pyplot.legend()
+pyplot.axis([0,10, 0, 13000])
+pyplot.xticks([1,2,3,4,5,6,7,8,9],['None', 'GZ', 'SNAPPY', 'PREFIX', 'PREFIX_TREE', 'DIFF', 'FAST_DIFF', 'ROW', 'ROWCOL' ], rotation=30)
+pyplot.savefig('results/experiment1/throughputs.png')
+pyplot.show()
