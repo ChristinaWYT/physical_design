26b890cc3c72016e8317e97b25345d72741a9eb2
abhishek <abhishek1015@gmail.com>
2014-07-09 22:01:26 -0700

    more results

---
+++ RunWorkloadcExperiment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+for i in `seq 34 48`;
+do
+	./deleteoldlogs.sh
+	start-dfs.sh
+	start-hbase.sh
+	./RunACase.sh clean create workloadc &>case`echo $i`.log
+	./collectcasetrace.sh case`echo $i`
+	stop-hbase.sh
+	stop-dfs.sh
+done
+
+set +x
